85b78c3f3e22c43ab58527672e4b39f4e79fd8b3
sri30281 <58740891+sri30281@users.noreply.github.com>
2021-08-24 10:37:20 +0530

    Update npmDay3.js

--- npmdemo/npmDay3.js
+++ npmdemo/npmDay3.js
@@ -1,1 +1,14 @@
+const moment =require('moment');
+const http=require('http');
+var server=http.createServer(function(req,res)
+{
+    res.write(moment().format('DD MMM YYYY, h:mm:ss a'));
+    res.end();
+})
+server.listen(5000,function(err)
+{
+    if(!err)
+    console.log('Server listening on port 3000');
+})
+
 
